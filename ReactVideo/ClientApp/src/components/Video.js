9656f2ceebdf02992fb3d22f13363199daf5d7e0
Cecil Lew <cecil.lew@appcentrica.com>
2018-04-16 10:25:36 -0400

    .

---
+++ ReactVideo/ClientApp/src/components/Video.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+import VideoPlayer from './VideoPlayer';
+
+export class Video extends Component {
+    render() {
+        return (
+            <VideoPlayer srcList={[
+                "https://localhost:44327/VideoLoader?filename=v1.mp4",
+                "https://localhost:44327/VideoLoader?filename=v2.mp4",
+                "https://localhost:44327/VideoLoader?filename=v3.mp4",
+                "https://localhost:44327/VideoLoader?filename=v4.mp4",
+                "https://localhost:44327/VideoLoader?filename=v5.mp4"
+            ]}
+                width="500px"
+            >
+            </VideoPlayer>
+        );
+    }
+}
---
+++ ReactVideo/Controllers/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace ReactImage.Controllers
+{
+    [Route("VideoLoader")]
+    public class VideoController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public VideoController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index(string filename, string accessKey)
+        {
+            System.Console.WriteLine(HttpContext.Request.Headers["Range"]);
+            var path = Path.Combine(_hostingEnvironment.ContentRootPath, @"media\" + filename);
+            return File(System.IO.File.OpenRead(path), "video/mp4", true); 
+        }
+    }
+}
